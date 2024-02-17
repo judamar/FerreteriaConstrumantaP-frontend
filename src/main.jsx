0d0962d000a4f600cbc5bbba99ec300af4ae0ecc
judamar <juancho.o.mart@gmail.com>
2024-02-16 23:43:26 -0500

    restart project

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import 'dotenv/config'
-import '@fortawesome/fontawesome-free/css/all.min.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.js'
-import axios from 'axios'
-
-window.axios = axios
-
-window.axios.defaults.baseURL = import.meta.env.API_URL;
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>,
 )
