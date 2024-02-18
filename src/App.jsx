235acf414acdeffc6bc4a035ee0d90bf63235ac1
judamar <juancho.o.mart@gmail.com>
2024-02-18 12:24:20 -0500

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import NavbarComponent from './components/Nav.jsx'
 
 function App() {
   return (
     <>
+      <NavbarComponent/>
       <h1>Test</h1>
     </>
   )
