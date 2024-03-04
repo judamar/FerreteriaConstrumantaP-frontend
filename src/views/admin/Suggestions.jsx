af8ab8850438cd3b74c4e225954b874cd4af3611
judamar <juancho.o.mart@gmail.com>
2024-03-04 00:16:06 -0500

    start suggestions

---
+++ src/views/admin/Suggestions.jsx
@@ -0,0 +1,11 @@
+import React,{ useEffect, useState, useRef } from 'react'
+import { confirmation, sendRequest } from '../../functions.jsx'
+
+const Suggestions = () => {
+  const [sugerencias, setSugerencias] = useState([])
+  return (
+    <div>Sugestions</div>
+  )
+}
+
+export default Sugestions
