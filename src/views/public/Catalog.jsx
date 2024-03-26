1890ab115c5c91edcbf981814649f15075d47334
judamar <juancho.o.mart@gmail.com>
2024-03-26 16:57:27 -0500

    catalog

--- src/views/public/Catalog.jsx
+++ src/views/public/Catalog.jsx
@@ -1,13 +1,17 @@
 import React, {useState, useEffect} from 'react'
 import ProductCard from '../../components/ProductCard'
+import DivSelect from '../../components/DivSelect'
 import { sendRequest } from '../../functions'
 
 const Catalog = () => {
   const [products, setProducts] = useState([])
+  const [categories, setCategories] = useState([])
+  const [categoria_id, setCategoria_id] = useState('')
+
 
   useEffect(() => {
     getProducts()
-    console.log(products)
+    getCategories()
   }, [])
 
   const getProducts = async () => {
@@ -15,9 +19,16 @@ const Catalog = () => {
     setProducts(res.data)
   }
 
+  const getCategories = async () => {
+    const res = await sendRequest('GET', '', '/categorias', '')
+    setCategories(res.data)
+  }
+
   return (
     <div className="container">
+      
       <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
+        <DivSelect icon='fa-tag' value={categoria_id} required='' className='form-select' options={categories} sel='categoria' placeholder='Filtrar por Categoria' handleChange={(e)=>setCategoria_id(e.target.value)}/>
         {products.map((product) => (
           <div key={product.id} className="col">
             <ProductCard product={product} />
