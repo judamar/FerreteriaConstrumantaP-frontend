d8e44e48654a16d69319ba865104889fe0f8ab41
judamar <juancho.o.mart@gmail.com>
2024-05-25 15:42:36 -0500

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import axios from 'axios'
 
 window.axios = axios
 
-window.axios.defaults.baseURL = import.meta.env.BACKEND_URL
+window.axios.defaults.baseURL = `${import.meta.env.BACKEND_URL}/api/`
 window.axios.defaults.headers.common['Accept'] = 'application/json'
 window.axios.defaults.headers.common['Content-Type'] = 'application/json'
 window.axios.defaults.headers.common['Accept'] = 'application/x-www-form-urlencoded'
