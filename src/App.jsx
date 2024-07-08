dbf8359526eeb605d2fd489bd0d23b3a9f9b1c06
judamar <juancho.o.mart@gmail.com>
2024-07-07 19:43:23 -0500

    add:catalog for / path

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,7 @@ function App() {
         <NavbarComponent/>
         <WhatsAppIn/>
         <Routes>
+          <Route path="/" element={<Catalog/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/register" element={<Register/>}/>
           <Route path="/catalogo" element={<Catalog/>}/>
